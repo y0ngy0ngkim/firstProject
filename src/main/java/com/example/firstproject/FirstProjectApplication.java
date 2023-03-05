b5133044ec418d5c9f1daa319d8770b25313995a
y0ngy0ngkim <yongchankimkim@gmail.com>
2023-03-05 23:38:22 +0900

    first commit

--- src/main/java/com/example/firstproject/FirstProjectApplication.java
+++ src/main/java/com/example/firstproject/FirstProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstProjectApplication {
 
     public static void main(String[] args) {
+        System.out.println("21");
         SpringApplication.run(FirstProjectApplication.class, args);
     }
 
